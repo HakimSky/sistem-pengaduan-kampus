df7780df14094133858791bb8fa0e27712d0b96c
HakimSky <l200220090@student.ums.ac.id>
2025-05-26 01:04:20 +0700

    akun pihak kampus, admin panel, pengaduan di pihak kampus update status

--- server/pihak_kampus/views.py
+++ server/pihak_kampus/views.py
@@ -1,11 +1,21 @@
 from rest_framework import viewsets
 from .models import PihakKampus, PihakKampusPengaduan
 from .serializers import PihakKampusSerializer, PihakKampusPengaduanSerializer
+from rest_framework.decorators import action
 
 class PihakKampusViewSet(viewsets.ModelViewSet):
     queryset = PihakKampus.objects.all()
     serializer_class = PihakKampusSerializer
-
 class PihakKampusPengaduanViewSet(viewsets.ModelViewSet):
     queryset = PihakKampusPengaduan.objects.all()
     serializer_class = PihakKampusPengaduanSerializer
+
+    @action(detail=True, methods=['patch'])
+    def update_status(self, request, pk=None):
+        pengaduan = self.get_object()
+        status_baru = request.data.get('status_kampus')
+        if status_baru in ['Menunggu', 'Diproses', 'Selesai']:
+            pengaduan.status_kampus = status_baru
+            pengaduan.save()
+            return Response({'message': 'Status updated!'})
+        return Response({'error': 'Status tidak valid'}, status=400)
